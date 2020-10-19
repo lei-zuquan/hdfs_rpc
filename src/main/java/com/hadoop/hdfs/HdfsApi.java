9898f36b7e1b63c32b7993a1b517904f6d224491
leizuquan <843291011@qq.com>
2020-10-19 12:47:12 +0800

    # 将测试接口迁移至Main类

--- src/main/java/com/hadoop/hdfs/HdfsApi.java
+++ src/main/java/com/hadoop/hdfs/HdfsApi.java
@@ -208,65 +208,6 @@ public class HdfsApi {
 		}
 	}
 
-	/**
-	 * 获取某个文件夹下的所有文件
-	 */
-	public static void getAllFileName(String path) throws Exception {
-		System.out.println(path);
-
-		if (!SRC_FILE_PATH.equals(path)){
-			String subFilePath = path.substring(SRC_FILE_PATH.length() + 1);
-			String fullFilePath = DEST_FILE_PATH + '/' + subFilePath;
-			mkdir(fullFilePath);
-		}
-
-		File file = new File(path);
-		File[] tempList = file.listFiles();
-
-		for (int i = 0; i < tempList.length; i++) {
-			if (tempList[i].isFile()) {
-				//System.out.println(tempList[i].getName());
-				//System.out.println(tempList[i].getPath());
-				String srcFilePath = tempList[i].getPath();
-				String subFilePath = srcFilePath.substring(SRC_FILE_PATH.length() + 1);
-				String fullFilePath = DEST_FILE_PATH + '/' + subFilePath;
-				copyFileToHDFS(tempList[i].getPath(), fullFilePath);
-			}
-			if (tempList[i].isDirectory()) {
-				getAllFileName(tempList[i].getAbsolutePath());
-			}
-		}
-		return;
-	}
-
-	private static String SRC_FILE_PATH = "F:\\ForLei\\法库\\法规分类";
-	private static String DEST_FILE_PATH = "/user/root/sqoop/faku";
-
-	public static void main(String[] args) throws IOException {
-		try {
-			HdfsApi.getFs();
-			copyFileToHDFS("d://test.jpg", "/user/bigdatadev/up_test.jpg");
-			FileStatus[] fileStatuses = listFile("/user/bigdatadev");
-			for (FileStatus fileName : fileStatuses){
-				System.out.println(fileName.getPath().getName());
-			}
-
-			delete("/user/bigdatadev/up_test.jpg");
-			fileStatuses = listFile("/user/bigdatadev");
-			for (FileStatus fileName : fileStatuses){
-				System.out.println(fileName.getPath().getName());
-			}
-//			deletedir(DEST_FILE_PATH,true);
-//			try {
-//				getAllFileName(SRC_FILE_PATH);
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-
-		} finally {
-			HdfsApi.closeFs();
-		}
-	}
 
 	private static void apiTest() throws Exception {
 		HdfsApi.getFs();
